ebd02f807ae896647cbd1e9433a1747357e975f5
nicky <nicky@nicky-ji.(none)>
2014-08-11 11:43:17 +0800

    first commit

---
+++ edx-platform/common/djangoapps/student/admin.py
@@ -0,0 +1,21 @@
+'''
+django admin pages for courseware model
+'''
+
+from student.models import UserProfile, UserTestGroup, CourseEnrollmentAllowed
+from student.models import CourseEnrollment, Registration, PendingNameChange, CourseAccessRole, CourseAccessRoleAdmin
+from ratelimitbackend import admin
+
+admin.site.register(UserProfile)
+
+admin.site.register(UserTestGroup)
+
+admin.site.register(CourseEnrollment)
+
+admin.site.register(CourseEnrollmentAllowed)
+
+admin.site.register(Registration)
+
+admin.site.register(PendingNameChange)
+
+admin.site.register(CourseAccessRole, CourseAccessRoleAdmin)
